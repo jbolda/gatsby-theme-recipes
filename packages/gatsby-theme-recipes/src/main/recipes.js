a1aa8d1b12d5f8a4b1b9e55e95fd89a7d43b01ca
Jacob Bolda <me@jacobbolda.com>
2020-02-28 00:27:57 -0600

    pull out helmet blocks

--- packages/gatsby-theme-recipes/src/main/recipes.js
+++ packages/gatsby-theme-recipes/src/main/recipes.js
@@ -1,94 +1,57 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { Helmet } from "react-helmet";
 import { Flex, Box, Heading, Link, Divider } from "theme-ui";
 import { Link as GatsbyLink } from "gatsby";
 
 import NavElement from "../components/navElement";
+import HelmetRecipes from "../components/helmetRecipes";
 import FeaturedImage from "../components/featuredImage";
 
 const RecipePage = props => {
   const recipes = props.data.allRecipes.edges;
   return (
-    <div>
-      <Helmet>
-        <title>Recipes</title>
-        <meta
-          name="description"
-          content="This is a list of all my favorite recipes."
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Recipes" />
-        <meta
-          property="og:url"
-          content={`${props.pageContext.siteUrl}${props.data.recipePage.path}`}
-        />
-        <meta
-          property="og:description"
-          content="This is a list of all my favorite recipes."
-        />
-        {!!recipes[0].node.featured_image &&
-        !!recipes[0].node.featured_image.fluid &&
-        !!recipes[0].node.featured_image.fluid.src ? (
-          <meta
-            property="og:image"
-            content={`${props.pageContext.siteUrl}${recipes[0].node.featured_image.fluid.src}`}
-          />
-        ) : null}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:domain" value={props.pageContext.siteUrl} />
-        <meta name="twitter:title" value="Recipes" />
-        <meta
-          name="twitter:description"
-          value="This is a list of all my favorite recipes."
-        />
-        {!!recipes[0].node.featured_image &&
-        !!recipes[0].node.featured_image.fluid &&
-        !!recipes[0].node.featured_image.fluid.src ? (
-          <meta
-            property="twitter:image"
-            content={`${props.pageContext.siteUrl}${recipes[0].node.featured_image.fluid.src}`}
-          />
-        ) : null}
-      </Helmet>
-      <NavElement
-        crumbs={[
-          <Link as={GatsbyLink} to="/">
-            Home
-          </Link>,
-          <Link as={GatsbyLink} to={props.data.recipePage.path}>
-            Recipes
-          </Link>
-        ]}
+    <NavElement
+      crumbs={[
+        <Link as={GatsbyLink} to="/">
+          Home
+        </Link>,
+        <Link as={GatsbyLink} to={props.data.recipePage.path}>
+          Recipes
+        </Link>
+      ]}
+    >
+      <HelmetRecipes
+        recipes={recipes}
+        siteUrl={props.pageContext.siteUrl}
+        recipePagePath={props.recipePage.path}
+      />
+      <Flex
+        sx={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "top"
+        }}
       >
-        <Flex
-          sx={{
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            alignItems: "top"
-          }}
-        >
-          {recipes.map(recipe => (
-            <Box
-              key={recipe.node.slug}
-              sx={{ width: ["95%", "75%", "25%"], padding: 3 }}
-            >
-              <Heading as="h2">
-                <Link as={GatsbyLink} to={recipe.node.slug}>
-                  {recipe.node.name}
-                </Link>
-              </Heading>
-              <FeaturedImage image={recipe.node.featured_image} />
-              <Heading as="h3">Ingredients</Heading>
-              <MDXRenderer>{recipe.node.ingredients.body}</MDXRenderer>
-              <Divider />
-            </Box>
-          ))}
-        </Flex>
-      </NavElement>
-    </div>
+        {recipes.map(recipe => (
+          <Box
+            key={recipe.node.slug}
+            sx={{ width: ["95%", "75%", "25%"], padding: 3 }}
+          >
+            <Heading as="h2">
+              <Link as={GatsbyLink} to={recipe.node.slug}>
+                {recipe.node.name}
+              </Link>
+            </Heading>
+            <FeaturedImage image={recipe.node.featured_image} />
+            <Heading as="h3">Ingredients</Heading>
+            <MDXRenderer>{recipe.node.ingredients.body}</MDXRenderer>
+            <Divider />
+          </Box>
+        ))}
+      </Flex>
+    </NavElement>
   );
 };
 
--- packages/gatsby-theme-recipes/src/templates/recipeTemplate.js
+++ packages/gatsby-theme-recipes/src/templates/recipeTemplate.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { Helmet } from "react-helmet";
 import { Flex, Box, Heading, Link } from "theme-ui";
 import { Link as GatsbyLink } from "gatsby";
 
 import NavElement from "../components/navElement";
+import HelmetRecipe from "../components/helmetRecipe";
 import FeaturedImage from "../components/featuredImage";
 import Details from "../components/details";
 import Inspiration from "../components/inspiration";
@@ -14,109 +14,64 @@ const SimpleRecipe = props => {
   const recipe = props.data.recipes;
 
   return (
-    <div>
-      <Helmet>
-        <title>{recipe.name}</title>
-        <meta
-          name="description"
-          content={`Check out the instructions to make ${recipe.name}.`}
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={recipe.name} />
-        <meta
-          property="og:url"
-          content={`${props.pageContext.siteUrl}${recipe.slug}`}
-        />
-        <meta
-          property="og:description"
-          content={`Check out the instructions to make ${recipe.name}.`}
-        />
-        {!!recipe.featured_image &&
-        !!recipe.featured_image.fluid &&
-        !!recipe.featured_image.fluid.src ? (
-          <meta
-            property="og:image"
-            content={`${props.pageContext.siteUrl}${recipe.featured_image.fluid.src}`}
-          />
-        ) : null}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:domain" value={props.pageContext.siteUrl} />
-        <meta name="twitter:title" value={recipe.name} />
-        <meta
-          name="twitter:description"
-          value={`Check out the instructions to make ${recipe.name}.`}
-        />
-        {!!recipe.featured_image &&
-        !!recipe.featured_image.fluid &&
-        !!recipe.featured_image.fluid.src ? (
-          <meta
-            property="twitter:image"
-            content={`${props.pageContext.siteUrl}${recipe.featured_image.fluid.src}`}
-          />
-        ) : null}
-        <meta name="twitter:label1" content="Total Time" />
-        <meta name="twitter:data1" content={recipe.total_time} />
-        <meta name="twitter:label2" content="Cook Time" />
-        <meta name="twitter:data2" content={recipe.cooking_time} />
-      </Helmet>
-      <NavElement
-        crumbs={[
-          <Link as={GatsbyLink} to="/">
-            Home
-          </Link>,
-          <Link as={GatsbyLink} to={props.data.recipePage.path}>
-            Recipes
-          </Link>,
-          <Link as={GatsbyLink} to={recipe.slug} aria-current="page">
-            {recipe.name}
-          </Link>
-        ]}
+    <NavElement
+      crumbs={[
+        <Link as={GatsbyLink} to="/">
+          Home
+        </Link>,
+        <Link as={GatsbyLink} to={props.data.recipePage.path}>
+          Recipes
+        </Link>,
+        <Link as={GatsbyLink} to={recipe.slug} aria-current="page">
+          {recipe.name}
+        </Link>
+      ]}
+    >
+      <HelmetRecipe recipe={recipe} siteUrl={props.pageContext.siteUrl} />
+      <Flex
+        sx={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
       >
-        <Flex
-          sx={{
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
+        <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
+          <Heading>{recipe.name}</Heading>
+          <FeaturedImage image={recipe.featured_image} />
+          <Details
+            items={[
+              { label: "Rating", detail: recipe.rating },
+              { label: "Last Made", detail: recipe.last_made },
+              { label: "Prep Time", detail: recipe.preparation_time },
+              { label: "Cook Time", detail: recipe.cooking_time },
+              { label: "Total Time", detail: recipe.total_time }
+            ]}
+          />
+        </Box>
+        {recipe?.ingredients?.body ? (
           <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
-            <Heading>{recipe.name}</Heading>
-            <FeaturedImage image={recipe.featured_image} />
-            <Details
-              items={[
-                { label: "Rating", detail: recipe.rating },
-                { label: "Last Made", detail: recipe.last_made },
-                { label: "Prep Time", detail: recipe.preparation_time },
-                { label: "Cook Time", detail: recipe.cooking_time },
-                { label: "Total Time", detail: recipe.total_time }
-              ]}
-            />
+            <Heading as={"h2"}>Ingredients</Heading>
+            <MDXRenderer>{recipe.ingredients.body}</MDXRenderer>
           </Box>
-          {recipe?.ingredients?.body ? (
-            <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
-              <Heading as={"h2"}>Ingredients</Heading>
-              <MDXRenderer>{recipe.ingredients.body}</MDXRenderer>
-            </Box>
-          ) : null}
-          {recipe?.directions?.body ? (
-            <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
-              <Heading as={"h2"}>Directions</Heading>
-              <MDXRenderer>{recipe.directions.body}</MDXRenderer>
-            </Box>
-          ) : null}
-          {recipe?.notes?.body ? (
-            <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
-              <Heading as={"h2"}>Directions</Heading>
-              <MDXRenderer>{recipe.directions.body}</MDXRenderer>
-            </Box>
-          ) : null}
+        ) : null}
+        {recipe?.directions?.body ? (
           <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
-            <Inspiration from={recipe.inspiration} />
+            <Heading as={"h2"}>Directions</Heading>
+            <MDXRenderer>{recipe.directions.body}</MDXRenderer>
           </Box>
-        </Flex>
-      </NavElement>
-    </div>
+        ) : null}
+        {recipe?.notes?.body ? (
+          <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
+            <Heading as={"h2"}>Directions</Heading>
+            <MDXRenderer>{recipe.directions.body}</MDXRenderer>
+          </Box>
+        ) : null}
+        <Box sx={{ width: ["95%", "85%", "50%"], padding: 3 }}>
+          <Inspiration from={recipe.inspiration} />
+        </Box>
+      </Flex>
+    </NavElement>
   );
 };
 
