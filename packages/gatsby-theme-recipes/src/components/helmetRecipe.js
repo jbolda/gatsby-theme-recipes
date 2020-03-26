6b75c8d9a1a995a8eaaf03adc54d299082ca183e
Jacob Bolda <me@jacobbolda.com>
2020-03-26 16:58:56 -0500

    fix: social image slash replace (#81)
    * replace double slash on full url
    
    * search for static to replace, will always be in static
    
    * check for image before replacing string
    
    * Create six-ads-doubt.md

--- packages/gatsby-theme-recipes/src/components/helmetRecipe.js
+++ packages/gatsby-theme-recipes/src/components/helmetRecipe.js
@@ -1,48 +1,44 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-export default ({ recipe, siteUrl }) => (
-  <Helmet>
-    <title>{recipe.name}</title>
-    <meta
-      name="description"
-      content={`Check out the instructions to make ${recipe.name}.`}
-    />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content={recipe.name} />
-    <meta property="og:url" content={`${siteUrl}${recipe.slug}`} />
-    <meta
-      property="og:description"
-      content={`Check out the instructions to make ${recipe.name}.`}
-    />
-    {!!recipe?.featured_image?.fluid?.src ? (
+export default ({ recipe, siteUrl }) => {
+  const socialImage = !!recipe?.featured_image?.fluid?.src
+    ? `${siteUrl}${recipe.featured_image.fluid.src}`.replace(
+        "//static",
+        "/static"
+      )
+    : null;
+  return (
+    <Helmet>
+      <title>{recipe.name}</title>
       <meta
-        property="og:image"
-        content={`${siteUrl}${recipe.featured_image.fluid.src}`.replace(
-          "//",
-          "/"
-        )}
+        name="description"
+        content={`Check out the instructions to make ${recipe.name}.`}
       />
-    ) : null}
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:domain" value={siteUrl} />
-    <meta name="twitter:title" value={recipe.name} />
-    <meta
-      name="twitter:description"
-      value={`Check out the instructions to make ${recipe.name}.`}
-    />
-    {!!recipe?.featured_image?.fluid?.src ? (
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={recipe.name} />
+      <meta property="og:url" content={`${siteUrl}${recipe.slug}`} />
       <meta
-        name="twitter:image"
-        content={`${siteUrl}${recipe.featured_image.fluid.src}`.replace(
-          "//",
-          "/"
-        )}
+        property="og:description"
+        content={`Check out the instructions to make ${recipe.name}.`}
       />
-    ) : null}
-    <meta name="twitter:label1" content="Total Time" />
-    <meta name="twitter:data1" content={recipe.total_time} />
-    <meta name="twitter:label2" content="Cook Time" />
-    <meta name="twitter:data2" content={recipe.cooking_time} />
-  </Helmet>
-);
+      {!!socialImage ? (
+        <meta property="og:image" content={socialImage} />
+      ) : null}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:domain" value={siteUrl} />
+      <meta name="twitter:title" value={recipe.name} />
+      <meta
+        name="twitter:description"
+        value={`Check out the instructions to make ${recipe.name}.`}
+      />
+      {!!socialImage ? (
+        <meta name="twitter:image" content={socialImage} />
+      ) : null}
+      <meta name="twitter:label1" content="Total Time" />
+      <meta name="twitter:data1" content={recipe.total_time} />
+      <meta name="twitter:label2" content="Cook Time" />
+      <meta name="twitter:data2" content={recipe.cooking_time} />
+    </Helmet>
+  );
+};
--- packages/gatsby-theme-recipes/src/components/helmetRecipes.js
+++ packages/gatsby-theme-recipes/src/components/helmetRecipes.js
@@ -1,44 +1,40 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-export default ({ recipes, siteUrl, recipePagePath }) => (
-  <Helmet>
-    <title>Recipes</title>
-    <meta
-      name="description"
-      content="This is a list of all my favorite recipes."
-    />
-    <meta property="og:type" content="website" />
-    <meta property="og:title" content="Recipes" />
-    <meta property="og:url" content={`${siteUrl}${recipePagePath}`} />
-    <meta
-      property="og:description"
-      content="This is a list of all my favorite recipes."
-    />
-    {!!recipes[0]?.node?.featured_image?.fluid?.src ? (
+export default ({ recipes, siteUrl, recipePagePath }) => {
+  const socialImage = !!recipes[0]?.node?.featured_image?.fluid?.src
+    ? `${siteUrl}${recipes[0].node.featured_image.fluid.src}`.replace(
+        "//static",
+        "/static"
+      )
+    : null;
+  return (
+    <Helmet>
+      <title>Recipes</title>
       <meta
-        property="og:image"
-        content={`${siteUrl}${recipes[0].node.featured_image.fluid.src}`.replace(
-          "//",
-          "/"
-        )}
+        name="description"
+        content="This is a list of all my favorite recipes."
       />
-    ) : null}
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:domain" value={siteUrl} />
-    <meta name="twitter:title" value="Recipes" />
-    <meta
-      name="twitter:description"
-      value="This is a list of all my favorite recipes."
-    />
-    {!!recipes[0]?.node?.featured_image?.fluid?.src ? (
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Recipes" />
+      <meta property="og:url" content={`${siteUrl}${recipePagePath}`} />
       <meta
-        name="twitter:image"
-        content={`${siteUrl}${recipes[0].node.featured_image.fluid.src}`.replace(
-          "//",
-          "/"
-        )}
+        property="og:description"
+        content="This is a list of all my favorite recipes."
       />
-    ) : null}
-  </Helmet>
-);
+      {!!socialImage ? (
+        <meta property="og:image" content={socialImage} />
+      ) : null}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:domain" value={siteUrl} />
+      <meta name="twitter:title" value="Recipes" />
+      <meta
+        name="twitter:description"
+        value="This is a list of all my favorite recipes."
+      />
+      {!!socialImage ? (
+        <meta name="twitter:image" content={socialImage} />
+      ) : null}
+    </Helmet>
+  );
+};
